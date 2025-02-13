854f566eac622d397a03e7ba2f72e42c44d2a2d8
ntende28 <ed25ntende@gmail.com>
2025-02-14 00:22:00 +0800

    learning about shell scripting

---
+++ sh/hello_world.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+read first_name
+age=25
+
+echo Hello $first_name, you are $age years old.
---
+++ sh/second.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+firstname=Eschobar
+lastname=Pablo
+age=45
+
+echo One of the greatest drug lords was names $lastname $firstname,  He gained prominence at $age
+
